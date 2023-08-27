ac6ddfc19498c90b45635522400e3a28d221f933
sowmik-sec <sowmik.sec@gmail.com>
2023-08-27 20:26:03 +0600

    new project

---
+++ src/test/java/com/sowmik/springboot/demo/myCoolApp/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sowmik.springboot.demo.myCoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
