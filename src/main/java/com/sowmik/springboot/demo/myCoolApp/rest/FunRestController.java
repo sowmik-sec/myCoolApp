4bf13f03c22d40ad3dbe390091d76f81736500fc
sowmik-sec <sowmik.sec@gmail.com>
2023-09-02 11:50:22 +0600

    inject custom application properties

--- src/main/java/com/sowmik/springboot/demo/myCoolApp/rest/FunRestController.java
+++ src/main/java/com/sowmik/springboot/demo/myCoolApp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.sowmik.springboot.demo.myCoolApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,16 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Today is a sunny day";
+    }
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInf() {
+        return "Coach:" + coachName + ", Team name: " + teamName;
     }
 }
